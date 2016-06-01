4e0e354929cee01bd90ef21bd7d1ab8d557b20ed
Mason Bartle <mason_bartle@brown.edu>
2016-06-01 16:59:56 -0400

    Committing in order to set up with github

--- GravitationalLensing/ViewController.swift
+++ GravitationalLensing/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("setting up a github repository")
     }
 
     override func didReceiveMemoryWarning() {
